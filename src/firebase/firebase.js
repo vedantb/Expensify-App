3144ece07010085932bca30bd62904066e31b354
Vedant Bhatt <vedantbhatt@gmail.com>
2017-10-12 02:10:17 -0700

    setup test database environment

---
+++ src/firebase/firebase.js
@@ -0,0 +1,99 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot) => {
+//         const expenses = [];
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//         });
+
+//         console.log(expenses);
+//     });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+//     console.log(expenses);
+// });
+
+// database.ref('expenses').push({
+//     description: 'Rent',
+//     note: '',
+//     amount: 109500,
+//     createdAt: 976123498763
+// });
+
+
+// database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// });
+
+// setTimeout(() => {
+//     database.ref('age').set(22);
+// }, 3500);
+
+// database.ref().set({
+//     name: 'Vedant Bhatt',
+//     age: 26,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software Developer',
+//         company: 'Yahoo'
+//     },
+//     location: {
+//         city: 'Mountain View',
+//         country: 'United States'
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((error) => {
+//     console.log('This failed', e);
+// });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'location/city': 'Seattle',
+//     'job/company': 'Amazon'
+// });
+
+// // database.ref().set('This is my data');
+
+// database.ref('age').set(27);
+// database.ref('location/city').set('Sunnyvale');
+
+// database.ref('attributes').set({
+//     height: 73,
+//     weight: 200
+// });
+
+// database.ref()
+//     .remove()
+//     .then(() => {
+//         console.log('Data was removed');
+//     }).catch((e) => {
+//         console.log('Did not remove data', e);
+//     });
---
+++ src/playground/promises.js
@@ -0,0 +1,16 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve('This is my resolved data');
+        reject('something went wrong');
+    }, 5000);
+});
+
+console.log('before');
+
+promise.then((data) => {
+    console.log('1', data);
+}).catch((error) => {
+    console.log('error:', error);
+});
+
+console.log('after');
